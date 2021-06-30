52633f37beee9ee2e6dd0fc10381897759d5b721
John R. D'Orazio <priest@johnromanodorazio.com>
2021-06-30 17:06:07 +0200

    start implementing i18next

--- src/taskpane/word.js
+++ src/taskpane/word.js
@@ -4,14 +4,38 @@
  */
 
 // images references in the manifest
-import "./common-office";
-
+//import "./common-office";
+import i18next from 'i18next'
+import Backend from 'i18next-http-backend'
+import jqueryI18next from 'jquery-i18next'
 /* global document, Office, Word */
 
 Office.onReady(info => {
   if (info.host === Office.HostType.Word) {
     document.getElementById("sideload-msg").style.display = "none";
     document.getElementById("app-body").style.display = "flex";
+    const lang = Office.context.displayLanguage;ù
+    const ns = "bibleget";
+    console.log("current Office display language = " + lang);
+    i18next.use(Backend).init({
+      backend: {
+        loadPath: `../../locales/${lang}/${ns}.json`
+      }
+    }, (err, t) => {
+      jqueryI18next.init(i18next, $, {
+        tName: 't', // --> appends $.t = i18next.t
+        i18nName: 'i18n', // --> appends $.i18n = i18next
+        handleName: 'localize', // --> appends $(selector).localize(opts);
+        selectorAttr: 'data-i18n', // selector for translating elements
+        targetAttr: 'i18n-target', // data-() attribute to grab target element to translate (if different than itself)
+        optionsAttr: 'i18n-options', // data-() attribute that contains options, will load/set if useOptionsAttr = true
+        useOptionsAttr: false, // see optionsAttr
+        parseDefaultValueFromContent: true // parses default values from content ele.val or ele.text
+      });
+
+      $('body').localize();
+
+    });
     document.getElementById("getByQuote").onclick = getByQuote;
     document.getElementById("getByKeyword").onclick = getByKeyword;
     document.getElementById("txtQuote").onkeyup = validateQuote;
